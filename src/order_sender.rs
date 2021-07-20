d56be7cf2221c1bc6bce78df6bb2f00ac93ce534
Sebastian Rollen <seb@overmu.se>
2021-07-20 13:05:06 -0400

    refactor order_sender into actor

--- src/order_sender.rs
+++ src/order_sender.rs
@@ -1,27 +1,24 @@
 use alpaca::orders::OrderIntent;
+use anyhow::Result;
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use std::time::Duration;
-use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::sync::mpsc;
 use tracing::{error, info};
 
-pub struct OrderSender {
+struct OrderSender {
     producer: FutureProducer,
-    order_queue: UnboundedReceiver<OrderIntent>,
+    receiver: mpsc::Receiver<OrderIntent>,
 }
 
 impl OrderSender {
-    pub fn new(producer: FutureProducer, order_queue: UnboundedReceiver<OrderIntent>) -> Self {
-        Self {
-            producer,
-            order_queue,
-        }
+    fn new(producer: FutureProducer, receiver: mpsc::Receiver<OrderIntent>) -> Self {
+        Self { producer, receiver }
     }
 
     #[tracing::instrument(skip(self))]
-    pub async fn run(mut self) {
+    pub async fn run(&mut self) {
         info!("Starting OrderSender");
-        let producer = self.producer;
-        while let Some(oi) = self.order_queue.recv().await {
+        while let Some(oi) = self.receiver.recv().await {
             info!("Sending order_intent {:?}", oi);
             let payload = serde_json::to_string(&oi);
             match payload {
@@ -29,7 +26,7 @@ impl OrderSender {
                     let record = FutureRecord::to("order-intents")
                         .key(&oi.symbol)
                         .payload(&payload);
-                    let send = producer.send(record, Duration::from_secs(0)).await;
+                    let send = self.producer.send(record, Duration::from_secs(0)).await;
                     if let Err((e, m)) = send {
                         error!("Error: {:?}\nMessage: {:?}", e, m)
                     }
@@ -40,5 +37,23 @@ impl OrderSender {
             }
         }
         info!("Ending OrderSender");
+    }
+}
+
+pub struct OrderSenderHandle {
+    sender: mpsc::Sender<OrderIntent>,
+}
+
+impl OrderSenderHandle {
+    pub fn new(producer: FutureProducer) -> Self {
+        let (sender, receiver) = mpsc::channel(8);
+        let mut actor = OrderSender::new(producer, receiver);
+        tokio::spawn(async move { actor.run().await });
+        Self { sender }
+    }
+
+    pub async fn send(&self, msg: OrderIntent) -> Result<()> {
+        self.sender.send(msg).await?;
+        Ok(())
     }
 }
